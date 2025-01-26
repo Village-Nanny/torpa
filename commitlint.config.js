b58718af239af9988385563daddb2701a6771a97
Chris Ozgo <chrisozgo99@gmail.com>
2025-01-26 00:47:12 -0500

    commitlint rules off

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,15 @@
-export default {};
+export default {
+  rules: {
+    // Disable all rules by setting them to 0 (off)
+    'type-enum': [0],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-case': [0],
+    'subject-case': [0],
+    'subject-empty': [0],
+    'subject-full-stop': [0],
+    'header-max-length': [0],
+    'body-leading-blank': [0],
+    'footer-leading-blank': [0],
+  },
+};
