4aeaa47766965d4364442b88b8ca5a01d6ba8359
harry <haristsitsirigos@gmail.com>
2025-02-17 10:16:26 +0200

    Email total score

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -5,6 +5,9 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(request: Request) {
   const { blendingScore, segmentingScore, email } = await request.json();
 
+  const totalScore = blendingScore.correct + segmentingScore.correct;
+  const totalQuestions = blendingScore.total + segmentingScore.total;
+
   try {
     await resend.emails.send({
       from: 'Torpa <onboarding@resend.dev>',
@@ -16,6 +19,7 @@ export async function POST(request: Request) {
         <ul>
           <li>Blending: ${blendingScore.correct}/${blendingScore.total}</li>
           <li>Segmenting: ${segmentingScore.correct}/${segmentingScore.total}</li>
+          <li>Total Score: ${totalScore}/${totalQuestions}</li>
         </ul>
       `,
     });
